a5254c58e4c49fddb64c7469e6871c834ef3a591
Modesty Ophelia Aguilar <ophelia.m.aguilar@gmail.com>
2021-11-24 13:55:12 +0100

    Update App.js
    I couldnt fix the button :(

--- App.js
+++ App.js
@@ -1,21 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, TextInput, Button, Image, Pressable } from "react-native";
 
 export default function App() {
+
+    // All Functions
+    var textInputValue = "Hello from the console"; 
+    const handlePress = () => console.log(textInputValue); 
+    const handleChangeText = () => console.log(textInputValue); 
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.container, styles.Button}>
+
+      <textElements style={{backgroundColor: 'lightblue'}}>
+      <h1 >Hello World!</h1>
+      <h2>First React Native App</h2>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+      </textElements>
+
       <StatusBar style="auto" />
-    </View>
+
+      <h3>What would you like to get out of this course?</h3>
+      <TextInput style={{width: 500, height: 150, backgroundColor: 'lightgrey' }} placeholder="Initial Text" onChangeText={handleChangeText} />
+
+      <h3>A gif describing how you are feeling about the course:</h3>
+
+      <Image
+        style={{width: 100, height: 100}}
+        source={{
+          uri: "https://c.tenor.com/JY2fRmOGB1UAAAAM/cheer-happy.gif",
+        }}
+      />
+
+<Button 
+title="Submit" 
+color="lightblue"
+onPress={handlePress}/>
+
+</View>
   );
 }
 
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'left',
     justifyContent: 'center',
   },
 });
